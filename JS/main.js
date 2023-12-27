349942bfbbf446da9299bc8cf72eaea4fb094285
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2023-12-27 16:28:10 -0500

    OVERFATE BETA v1.6.0

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
 //VARIABLES
 const canvas = document.querySelector('canvas')
 var gravityaccy = 0.3;
+var gravityaccx = 0.3;
 let atk = false
 let counter = 0
 let counteralt = 0
@@ -8,6 +9,7 @@ let Player2pres;
 let Player1pres;
 let key
 let isfinishedT = false
+let ispaused = false
 let isfinishedP = false
 let alertcounter = 0
 let alrtcount2 = 0
@@ -19,7 +21,7 @@ var SRCPl2 = '../Addons/Sprites/PL1/Idle.png'
 var FRPL1;
 var FRPL2;
 var bglink
-let atkcount = 0
+let atkcount1 = 0
 var bool = {
     a:{
         press:false
@@ -56,7 +58,7 @@ var background = new Sprite({
         x:0,
         y:0
     },
-    imageSRC: bglink,
+    imageSRC: '../Addons/Background/BACKG1.gif',
     CurrentFrame: 0
 })
  
@@ -69,20 +71,27 @@ velocity: {
     x:0,
     y:0
 },
-color: 'green',
 lastpress: '',
 width: 50,
 height: 100,
-AttackBoxcolor: 'red',
 HP: 100,
 imageSRC: SRCPl1,
 FramesMax:1,
 isattack: false,
-increm: 70,
+increm: null,
 canattack:true,
 lastattack: false,
 iscombo:false,
-Framescount: 0
+Framescount: 0,
+Char: 'PL2',
+ivalue: null,
+jump: null,
+hit: false,
+fall: {
+    x: 0,
+    y: 0,
+},
+rotate: false
 }
 )
 
@@ -98,25 +107,41 @@ velocity: {
 },
 width: 50,
 height: 100,
-color: 'red',
 lastpress: '',
-AttackBoxcolor: 'yellow',
 HP: 100,
 imageSRC: SRCPl2,
 FramesMax:1,
 isattack: false,
 increm: 70,
-Framescount: 0
+Framescount: 0,
+Char: 'PL1',
+ivalue: null,
+jump: null,
+hit: false,
+fall: {
+    x: 0,
+    y: 0,
+},
+rotate: false
 })
+
+
+        Player1.increm = Skin(Player1.Char)[2] || 70
+        Player2.increm = Skin(Player2.Char)[2] || 70
+
+        Player1.jump = Skin(Player1.Char)[4] || 200;
+        Player1.ivalue = Skin(Player1.Char)[3] || 70
+
+        Player2.jump = Skin(Player2.Char)[4] || 200;
+        Player2.ivalue = Skin(Player2.Char)[3] || 70
+
+
 
 //KEY HANDLERS 
 var Lastrelease = 0
-if(!isfinishedT && !isfinishedP){
-
-    console.log(isfinishedP, isfinishedT)
 window.addEventListener('keydown', (event) => {
     
-    if(!isfinishedT && !isfinishedP){
+    if(!isfinishedT && !isfinishedP && !ispaused){
     if(event.key.length === 1){
         key = event.key.toLowerCase()}
         else{
@@ -134,8 +159,9 @@ window.addEventListener('keydown', (event) => {
                 Player1.velocity.y = -15
             }else Player1.velocity.y = -20
             console.log('true')
-            Player1.increm = 70
-            gravityaccy = 0.7
+            if(!Player1.isattack){
+            Player1.increm = 70}
+            gravityaccy = 0.9
             bool.w.press = true
             Player1.lastpress = 'w'
             }}
@@ -151,6 +177,7 @@ window.addEventListener('keydown', (event) => {
         }break;
 
         case 'd': {
+            //Player1.increm = 48
             bool.d.press = true
             Player1.lastpress = 'd'
         }break;
@@ -160,16 +187,18 @@ window.addEventListener('keydown', (event) => {
             if(!Player1.isattack){
             bool.e.press = true
             Player1.isattack = true;
-            Player1.lastpress = 'e'
             Player1.Framescount = 0
             Player1.increm = 70}
+            Player1.lastpress = 'e'
         }break;
         
         case 'Enter': {
-            if(counteralt < 5){
-                bool.ENTER.press = true}
-                counteralt++
-            Player2.lastpress = 'ENTER'
+            if(!Player2.isattack){
+                bool.ENTER.press = true
+                Player2.isattack = true;
+                Player2.lastpress = 'ENTER'
+                Player2.Framescount = 0
+                Player2.increm = 70}
         }break;
 
         case 'ArrowLeft': {
@@ -201,12 +230,22 @@ window.addEventListener('keydown', (event) => {
             
         }break;
 
-        
+        case 'Escape': {
+            if(document.getElementById('pause').style.display !== 'block'){
+            document.getElementById('pause').style.display = 'block';
+            ispaused = true}
+        }
 
         default: {
             console.log('incorrect event')
         }break
     }}
+    else{
+        if(event.key === 'Escape' || event.key.toLowerCase() === 'p'){
+            document.getElementById('pause').style.display = 'none';
+                ispaused = false
+        }
+    }
 })
 
 window.addEventListener('keyup', (event) => {
@@ -220,12 +259,14 @@ window.addEventListener('keyup', (event) => {
     switch(key){
         case 'w': {
             //jump
-            Player1.increm = 70
+            if(!Player1.isattack){
+            Player1.increm = 70}
             bool.w.press = false
         }break;
 
         case 'a': {
-            Player1.increm = 70
+            if(!Player1.isattack){
+            Player1.increm = 70}
             bool.a.press = false
             Player1.velocity.x = 0
             
@@ -236,7 +277,8 @@ window.addEventListener('keyup', (event) => {
         }break;
 
         case 'd': {
-            Player1.increm = 70
+            if(!Player1.isattack){
+            Player1.increm = 70}
             bool.d.press = false
             Player1.velocity.x = 0
             Player1.FramesMax = 8;
@@ -247,19 +289,18 @@ window.addEventListener('keyup', (event) => {
             bool.e.press = false
             counter = 0
             Player1.canattack = false
-            setTimeout(() => {
-                Player1.isattack = false;
-                bool.e.press = false
-            }, 100)
-            atkcount = 0
             }
             Lastrelease = Date.now()
 
         }break;
 
         case 'Enter': {
-            bool.ENTER.press = false
-            counteralt = 0
+            if(Player2.canattack){
+                bool.ENTER.press = false
+                counteralt = 0
+                Player2.canattack = false
+                }
+                Lastrelease = Date.now()
             
         }break;
 
@@ -279,5 +320,5 @@ window.addEventListener('keyup', (event) => {
             console.log('incorrect event')
         }break
     }}
-})}
+})
 anim()
